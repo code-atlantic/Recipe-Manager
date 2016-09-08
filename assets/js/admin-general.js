98741aeb421015961e501697e6b90216081fdd79
Daniel Iser <danieliser@wizardinternetsolutions.com>
2016-09-08 03:46:03 -0400

    Update build scripts & langpack.

--- assets/js/admin-general.js
+++ assets/js/admin-general.js
@@ -1,10 +1,42 @@
+var rcpm_selectors;
 (function ($) {
-    $(document).ready(function () {
-        var color_picker = $('.rcpm-color-picker');
+    'use strict';
 
-        if( color_picker.length ) {
-            color_picker.wpColorPicker();
-        }
-    });
+    function Selector_Cache() {
+        var elementCache = {};
+
+        var get_from_cache = function (selector, $ctxt, reset) {
+
+            if ('boolean' === typeof $ctxt) {
+                reset = $ctxt;
+                $ctxt = false;
+            }
+            var cacheKey = $ctxt ? $ctxt.selector + ' ' + selector : selector;
+
+            if (undefined === elementCache[cacheKey] || reset) {
+                elementCache[cacheKey] = $ctxt ? $ctxt.find(selector) : jQuery(selector);
+            }
+
+            return elementCache[cacheKey];
+        };
+
+        get_from_cache.elementCache = elementCache;
+        return get_from_cache;
+    }
+
+    rcpm_selectors = new Selector_Cache();
+
+    rcpm_selectors(document)
+        .ready(function () {
+            var color_picker = rcpm_selectors('.rcpm-color-picker');
+
+            if (color_picker.length) {
+                color_picker.wpColorPicker({
+                    change: function (event, ui) {
+                        rcpm_selectors('#' + event.target.id).trigger('colorchange', ui);
+                    }
+                });
+            }
+        });
 
 }(jQuery));
