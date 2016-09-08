170451c1c2039bb13cf3adcaac5e03bcc2cb8fbe
Daniel Iser <danieliser@wizardinternetsolutions.com>
2016-09-08 03:36:13 -0400

    Added rcpm_selectors global JS that provides query caching for optimized JS. Updated color pickers with a new event since IRIS hijacks 'change'.

--- assets/js/src/rcpm.js
+++ assets/js/src/rcpm.js
@@ -1,7 +1,29 @@
+var rcpm_selectors;
 (function ($, document) {
+    'use strict';
 
-    $(document).on('click', '.recipe-ingredient, .recipe-step', function () {
-        $(this).toggleClass('checked');
-    }).css({cursor:"pointer"});
+    function Selector_Cache() {
+        var elementCache = {};
+
+        var get_from_cache = function( selector, $ctxt, reset ) {
+
+            if ( 'boolean' === typeof $ctxt ) {
+                reset = $ctxt;
+                $ctxt = false;
+            }
+            var cacheKey = $ctxt ? $ctxt.selector + ' ' + selector : selector;
+
+            if ( undefined === elementCache[ cacheKey ] || reset ) {
+                elementCache[ cacheKey ] = $ctxt ? $ctxt.find( selector ) : jQuery( selector );
+            }
+
+            return elementCache[ cacheKey ];
+        };
+
+        get_from_cache.elementCache = elementCache;
+        return get_from_cache;
+    }
+
+    rcpm_selectors = new Selector_Cache();
 
 }(jQuery, document));
