724c82fbf248997a89c0cb346da88471f05852b4
Daniel Iser <danieliser@wizardinternetsolutions.com>
2016-09-08 03:31:24 -0400

    Moved admin-general scripts to be easily reusable.

--- classes/Admin/Assets.php
+++ classes/Admin/Assets.php
@@ -21,12 +21,19 @@ class Assets {
 
 		// Use minified libraries if SCRIPT_DEBUG is turned off
 		$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
-		if ( $hook == 'recipe_page_settings' ) {
-			wp_enqueue_style( 'dashicons-picker', RCPM::$URL . 'assets/css/dashicons-picker' . $suffix . '.css', array( 'dashicons' ), '1.0', false );
-			wp_enqueue_script( 'dashicons-picker', RCPM::$URL . 'assets/js/vendor/dashicons-picker' . $suffix . '.js', array( 'jquery' ), '1.1', true );
 
-			wp_enqueue_style( 'rcpm-admin-general', RCPM::$URL . 'assets/css/admin-general' . $suffix . '.css', array( 'dashicons' ), '1.0', false );
-			wp_enqueue_script( 'rcpm-admin-general', RCPM::$URL . 'assets/js/admin-general' . $suffix . '.js', array( 'jquery' ), RCPM::$VER, true );
+
+		wp_register_style( 'dashicons-picker', RCPM::$URL . 'assets/css/dashicons-picker' . $suffix . '.css', array( 'dashicons' ), '1.0', false );
+		wp_register_script( 'dashicons-picker', RCPM::$URL . 'assets/js/vendor/dashicons-picker' . $suffix . '.js', array( 'jquery' ), '1.1', true );
+
+		wp_register_style( 'rcpm-admin-general', RCPM::$URL . 'assets/css/admin-general' . $suffix . '.css', array( 'dashicons', 'dashicons-picker' ), '1.0', false );
+		wp_register_script( 'rcpm-admin-general', RCPM::$URL . 'assets/js/admin-general' . $suffix . '.js', array( 'jquery', 'dashicons-picker' ), RCPM::$VER, true );
+
+		if ( $hook == 'recipe_page_rcpm-settings' ) {
+			wp_enqueue_style( 'dashicons-picker' );
+			wp_enqueue_script( 'dashicons-picker' );
+			wp_enqueue_style( 'rcpm-admin-general' );
+			wp_enqueue_script( 'rcpm-admin-general' );
 			wp_enqueue_style( 'wp-color-picker' );
 			wp_enqueue_script( 'wp-color-picker' );
 		}
@@ -35,14 +42,9 @@ class Assets {
 
 			add_action( 'print_media_templates', array( '\\RCPM\Admin\Recipes\Templates', 'admin_footer' ) );
 
-			wp_enqueue_style( 'recipe-editor', RCPM::$URL . 'assets/css/admin-recipe-editor' . $suffix . '.css', null, RCPM::$VER );
-			wp_enqueue_script( 'recipe-editor', RCPM::$URL . 'assets/js/admin-recipe-editor' . $suffix . '.js', array(
-				'jquery',
-				'jquery-ui-sortable',
-				'jquery-ui-autocomplete',
-				'jquery-ui-spinner',
-			), RCPM::$VER );
-			wp_localize_script( 'recipe-editor', 'rcpm_recipe_editor_vars', apply_filters( 'rcpm_recipe_editor_script_vars', array(
+			wp_enqueue_style( 'rcpm-recipe-editor', RCPM::$URL . 'assets/css/admin-recipe-editor' . $suffix . '.css', array( 'rcpm-admin-general' ), RCPM::$VER );
+			wp_enqueue_script( 'rcpm-recipe-editor', RCPM::$URL . 'assets/js/admin-recipe-editor' . $suffix . '.js', array( 'jquery', 'jquery-ui-sortable', 'jquery-ui-autocomplete', 'jquery-ui-spinner', 'rcpm-admin-general' ), RCPM::$VER );
+			wp_localize_script( 'rcpm-recipe-editor', 'rcpm_recipe_editor_vars', apply_filters( 'rcpm_recipe_editor_script_vars', array(
 				'ingredients'       => rcpm_get_all_ingredients( array( 'ID', 'post_title as value' ) ),
 				'measurement_units' => array(
 					'singular' => rcpm_measurement_units( 'singular' ),
@@ -58,11 +60,8 @@ class Assets {
 
 		if ( 'ingredient' == $post_type && in_array( $hook, array( 'post.php', 'post-new.php' ) ) ) {
 
-			wp_enqueue_style( 'ingredient-editor', RCPM::$URL . 'assets/css/admin-ingredient-editor' . $suffix . '.css', null, RCPM::$VER );
-			wp_enqueue_script( 'ingredient-editor', RCPM::$URL . 'assets/js/admin-ingredient-editor' . $suffix . '.js', array(
-				'jquery',
-				'jquery-ui-tooltip',
-			), RCPM::$VER );
+			wp_enqueue_style( 'rcpm-ingredient-editor', RCPM::$URL . 'assets/css/admin-ingredient-editor' . $suffix . '.css', array( 'rcpm-admin-general' ), RCPM::$VER );
+			wp_enqueue_script( 'rcpm-ingredient-editor', RCPM::$URL . 'assets/js/admin-ingredient-editor' . $suffix . '.js', array( 'jquery', 'jquery-ui-tooltip', 'rcpm-admin-general' ), RCPM::$VER );
 			wp_localize_script( 'ingredient-editor', 'rcpm_ingredient_editor_vars', apply_filters( 'rcpm_ingredient_editor_script_vars', array(
 				'I10n' => array(),
 			) ) );
